f3fe8ef5b27ffe57b28ba72bd01e51ea1a339c40
Gregory Schoppe <gschoppe@gmail.com>
2018-01-18 20:43:18 -0500

    themes and separators

--- README.md
+++ README.md
@@ -2,27 +2,62 @@
 Create beautiful Slabbed typography effects, right from the WordPress editor.
 
 ## Examples
-![Sample Slabs created with WP jSlabify](https://i.imgur.com/pLF2FnR.png "Examples")
+![Sample Slabs created with WP jSlabify](https://i.imgur.com/VWx382O.png "Examples")
 
-### Usage
-#### Use Case 1: Auto Slabbing
+## Usage
+### Meet The Shortcodes
+
+#### `[slab]insert content here[/slab]`
+The *[slab]* shortcode is the basis of all slabbed typography. In fact, The other shortcodes will only function when wrapped in a *[slab]* shortcode. the slab shortcode offers the following optional attributes:
+
+* **theme** - apply templated styles. default options are *league* or *ultra*
+* **ratio** - the target ratio (width/height) of the resulting slab (this will effect how line lengths are selected, when slabbing text automatically). All values must be greater than zero. values greater than 1 will attempt to create slabs that are wider than they are tall. values less than 1 will attempt to create slabs that are taller than they are wide. Defaults to *1*.
+* **force** - when creating a slab, force the container's dimensions to match the proportion set in the "ratio" attribute, even if the slab content will not completely fill the container.
+* **hcenter** - when force is set to true, setting *hcenter* to true will center the slab horizontally within the container. defaults to *true*.
+* **vcenter** - when force is set to true, setting *vcenter* to true will center the slab vertically within the container. defaults to *true*.
+* **element** - The HTML element that will be used to wrap the slabbed text. defaults to *div*.
+* **id** - the id attribute to apply to the slab
+* **class** - a set of classes to apply to the slab
+* **style** - inline CSS to apply to the slab
+* **href** - (only available when element is set to "a") the url to direct users to, on click.
+* **title** - (only available when element is set to "a") the title of the link element.
+* **target** - (only available when element is set to "a") the target of the link element.
+
+#### `[slabline]insert content here[/slabline]`
+The *[slabline]* shortcode is used to manually group a set of words in a slab into a single row. it offers the following optional attributes:
+
+* **id** - the id attribute to apply to the line
+* **class** - a set of classes to apply to the line
+* **style** - inline CSS to apply to the line
+
+#### `[slabbreak]`
+The *[slabbreak]* shortcode is used to insert a separator between two lines in a slab. it offers the following optional attributes:
+
+* **type** - one of several predefined types of separators, including arrows and banners
+* **id** - the id attribute to apply to the separator
+* **class** - a set of classes to apply to the separator
+* **style** - inline CSS to apply to the separator
+
+## Examples
+### Auto Slabbing
 Auto slabbing is the easiest way to use the plugin. Simply set a target ratio for the slab (width/height), and the algorithm does the rest. However, it is the hardest to customize
 ```
-[slab element="h3" ratio="3"]
+[slab element="h3" ratio="3" theme="ultra"]
 The curious incident of the dog in the night
 [/slab]
 ```
-#### Use Case 2: Manual Slabbing
+### Manual Slabbing
 Manual Slabbing gives you the most control over the slab's layout and styling, but can be time-consuming.
 ```
-[slab element="blockquote"]
+[slab element="blockquote" theme="league"]
 [slabline style="color: red;"]The curious[/slabline]
 [slabline]incident of the dog[/slabline]
+[slabbreak type="banner"]
 [slabline class="lastline"]in the night[/slabline]
 [/slab]
 ```
 
-### Use Case 3: Mixed Slabbing
+### Mixed Slabbing
 Mixed Slabbing can provide the best of both worlds, allowing the algorithm to map most rows, but still allowing specific lines to be emphasized, as needed.
 ```
 [slab element="a" ratio="2" href="https://www.google.com"]
@@ -30,20 +65,3 @@ Mixed Slabbing can provide the best of both worlds, allowing the algorithm to ma
 of the dog in the night
 [/slab]
 ```
-
-## Options
-
-### [slab] options
-* **element** - The HTML element that will be used to wrap the slabbed text
-* **id** - the id attribute to apply to the slab
-* **class** - a set of classes to apply to the slab
-* **style** - inline CSS to apply to the slab
-* **ratio** - the target ratio (width/height) of the slab (normally only applies to manual or mixed slabbing)
-* **href** - (only available when element is set to "a") the url to direct users to, on click.
-* **title** - (only available when element is set to "a") the title of the link element.
-* **target** - (only available when element is set to "a") the target of the link element.
-
-### [slabline] options
-* **id** - the id attribute to apply to the line
-* **class** - a set of classes to apply to the line
-* **style** - inline CSS to apply to the line
--- wp-jslabify.php
+++ wp-jslabify.php
@@ -15,7 +15,9 @@ if( !class_exists('WPJSlabify') ) {
 		public $plugin_url;
 
 		private $in_slab = false;
+		private $in_line = false;
 		private $do_js   = false;
+		private $themes  = array();
 
 		public static function Instance() {
 			static $instance = null;
@@ -28,10 +30,13 @@ if( !class_exists('WPJSlabify') ) {
 		private function __construct() {
 			$this->plugin_dir = plugin_dir_path( __FILE__ );
 			$this->plugin_url = plugin_dir_url( __FILE__ );
+			add_filter( 'extra_wp_jslabify_custom_theme_headers', array( $this, 'wp_jslabify_custom_theme_headers' ) );
+			add_action( 'init', array( $this, 'enumerate_themes' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_resources' ) );
 			add_action( 'wp_footer', array( $this, 'maybe_do_js' ) );
 			add_shortcode( 'slab', array( $this, 'shortcode_slab' ) );
 			add_shortcode( 'slabline', array( $this, 'shortcode_slabline' ) );
+			add_shortcode( 'slabbreak', array( $this, 'shortcode_slabbreak' ) );
 		}
 
 		public function enqueue_resources() {
@@ -53,8 +58,15 @@ if( !class_exists('WPJSlabify') ) {
 				'href'    => '',
 				'title'   => '',
 				'target'  => '',
-
+				'theme'   => ''
 			), $atts, 'slab' );
+
+			// handle theme
+			if( $atts['theme'] ) {
+				$atts['class'] = trim( 'theme-' . sanitize_title( $atts['theme'] ) . ' ' . $atts['class'] );
+				$this->load_theme( $atts['theme'] );
+				unset( $atts['theme'] );
+			}
 
 			// add the standard class
 			$atts['class'] = trim( 'jslabify ' . $atts['class'] );
@@ -108,6 +120,14 @@ if( !class_exists('WPJSlabify') ) {
 				'style'   => '',
 			), $atts, 'slabline' );
 
+			$this->in_line = true;
+			$content = do_shortcode( $content );
+			$this->in_line = false;
+
+			if( !$this->in_slab ) {
+				return $content;
+			}
+
 			// add the standard class
 			$atts['class'] = trim( 'slabbedtext ' . $atts['class'] );
 
@@ -126,9 +146,44 @@ if( !class_exists('WPJSlabify') ) {
 			return $output;
 		}
 
+		public function shortcode_slabbreak( $atts, $content = '' ) {
+			$atts = shortcode_atts( array(
+				'id'      => '',
+				'class'   => '',
+				'style'   => '',
+				'type'    => 'default'
+			), $atts, 'slabline' );
+
+			if( !$this->in_slab || $this->in_line ) {
+				return $content;
+			}
+
+			if( $atts['type'] ) {
+				$atts['class'] = trim( 'slabbreak-' . sanitize_title( $atts['type'] ) . ' ' . $atts['class'] );
+				unset( $atts['type'] );
+			}
+
+			// add the standard class
+			$atts['class'] = trim( 'slabbedtext ' . $atts['class'] );
+
+			// get rid of any empty elements
+			$atts = array_filter( $atts );
+
+			// build the tag
+			$output  = '<span';
+			foreach( $atts as $att => $val ) {
+				$output .= ' ' . $att . '="' . esc_attr( $val ) . '"';
+			}
+			$output .= '></span>';
+
+			return $output;
+		}
+
 		public function maybe_do_js() {
 			if( $this->do_js ) {
-				$default_options = apply_filters( 'jslabify-default-options', array() );
+				$default_options = apply_filters( 'jslabify-default-options', array(
+					'postTweak' => false
+				) );
 				?>
 				<script>
 				jQuery(function($) {
@@ -150,6 +205,81 @@ if( !class_exists('WPJSlabify') ) {
 				</script>
 				<?php
 			}
+		}
+
+		public function load_theme( $theme_slug ) {
+			if( isset( $this->themes[ $theme_slug ] ) ) {
+				$theme = $this->themes[ $theme_slug ];
+				wp_enqueue_style( 'wp-jslabify-' . $theme_slug, $theme['URL'], array(), $theme['Version'] );
+			}
+		}
+
+		public function wp_jslabify_custom_theme_headers( $headers ) {
+			$headers[] = "Theme Name";
+			$headers[] = "Theme URI";
+			$headers[] = "Slug";
+			$headers[] = "Description";
+			$headers[] = "Icon";
+			$headers[] = "Version";
+			$headers[] = "Author";
+			$headers[] = "Author URI";
+			$headers[] = "License";
+			$headers[] = "License URI";
+			return $headers;
+		}
+
+		public function enumerate_themes() {
+			if( $this->themes ) {
+				return;
+			}
+			$template_dir = get_template_directory();
+			$stylesheet_dir = get_stylesheet_directory();
+			$theme_directories = array(
+				$this->plugin_dir . 'themes' => $this->plugin_url . 'themes'
+			);
+			$additional_dirs = apply_filters( 'jslabify-theme-folders', array() );
+			$theme_directories = array_merge( $theme_directories, $additional_dirs );
+			$theme_directories[ $template_dir . '/wp-jslabify' ] = get_template_directory_uri() . '/wp-jslabify';
+			if( $template_dir != $stylesheet_dir ) {
+				$theme_directories[ $stylesheet_dir . '/wp-jslabify' ] = get_stylesheet_directory_uri() . '/wp-jslabify';
+			}
+			$themes = array();
+			foreach( $theme_directories as $path=>$url ) {
+				$theme_set = $this->scan_folder_for_themes( $path, $url );
+				$themes = array_merge( $themes, $theme_set );
+			}
+			$this->themes = $themes;
+		}
+
+		private function scan_folder_for_themes( $path, $url ) {
+			$themes = array();
+			if( is_dir( $path ) ) {
+				$files = $this->rglob( $path, '*.css' );
+				foreach( $files as $file ) {
+					$file_url = str_replace( $path, $url, $file );
+					$header = get_file_data( $file, array(), "wp_jslabify_custom_theme" );
+					if( isset( $header['Theme Name'] ) && $header['Theme Name'] ) {
+						if( isset( $header['Slug'] ) && $header['Slug'] ) {
+							$slug = sanitize_title( $header['Slug'] );
+						} else {
+							$slug = sanitize_title( $header['Theme Name'] );
+						}
+						$slug = str_replace( '-', '_', $slug );
+						$header['File'] = $file;
+						$header['URL'] = $file_url;
+						$themes[ $slug ] = $header;
+					}
+				}
+			}
+			return $themes;
+		}
+
+		private function rglob($dir, $pattern, $flags = 0) {
+			$files = glob( $dir . '/' . $pattern, $flags );
+			foreach( glob( $dir.'/*', GLOB_ONLYDIR|GLOB_NOSORT ) as $dir ) {
+				$files = array_merge( $files, $this->rglob( $dir, $pattern, $flags ) );
+			}
+			return $files;
 		}
 	}
 	WPJSlabify::Instance();
